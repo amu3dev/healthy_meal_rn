d3fe85493320c673f67b0dd95befed293d89e04b
Ahmed Yosry <amu3dev@gmail.com>
2026-06-01 16:22:41 +0300

    chore: configure Jest for React Native with testing library and async-storage mock

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  testEnvironment: 'node',
+  preset: 'react-native',
   watchman: false,
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/__tests__/**/*.test.js'],
   moduleFileExtensions: ['js', 'json'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native|react-native|@react-navigation|react-native-safe-area-context|@testing-library/react-native)/)',
+  ],
 };
