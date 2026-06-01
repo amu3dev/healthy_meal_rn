d4e36aa8fb3e2ec7a4065274b4379b57bd1c6f53
Ahmed Yosry <amu3dev@gmail.com>
2026-06-01 16:47:50 +0300

    feat: add expo-image dependency and configure jest mock for testing support

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,16 @@ jest.mock(
   '@react-native-async-storage/async-storage',
   () => require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
+
+jest.mock('expo-image', () => {
+  const React = require('react');
+  const { Image } = require('react-native');
+
+  return {
+    Image: ({ source, style, ...props }) => React.createElement(Image, {
+      source,
+      style,
+      ...props,
+    }),
+  };
+});
